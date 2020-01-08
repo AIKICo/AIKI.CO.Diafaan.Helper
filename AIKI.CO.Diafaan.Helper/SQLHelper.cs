6744f3a81a5e2cb2785e6fabf47b33082f17df51
Mohammad Mehrnia <qermezkon@gmail.com>
2020-01-08 21:18:50 +0330

    Add project files.

---
+++ AIKI.CO.Diafaan.Helper/SQLHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AIKI.CO.Diafaan.Helper
+{
+    public static class SQLHelper
+    {
+        public static string ServerName { get; set; }
+        public static string  DBUserName { get; set; }
+        public static string DBPassword { get; set; }
+    }
+}
